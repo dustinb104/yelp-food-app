8b66ce35e0925306ffd7f680016452dacf6245d9
Dustin <dcbell3@uno.edu>
2021-10-07 13:34:53 -0500

    Second Test Commit

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 // This is a test commit
+// this is another test commit
 export default function App() {
   return (
     <View style={styles.container}>
