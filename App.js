c1bfaa14264cb2db3fc818b28980df48c9930a2f
Dustin <dcbell3@uno.edu>
2021-10-08 11:38:02 -0500

    importing navigation from react libraries

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from "react-navigation";
+import {createStackNavigator} from "react-navigation-stack";
 
-// This is a test commit
-// this is another test commit
+// Client ID
+// a6E3eWhCJVWkV5pzR8dqww
+
+// API Key
+// 1VRIbiqBtrDHiYrYmvcFOePyr3vPOICHG0IsuvH3W4nlEo1645l2_abRkJhcQtJWE1XDntzSwd0u1VgaXRZe_691he4lPBemuT2hrB6e8ogOfVzYkGTAg59mVr1fYXYx
+
 export default function App() {
   return (
     <View style={styles.container}>
